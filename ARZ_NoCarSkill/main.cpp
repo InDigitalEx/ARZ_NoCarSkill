872863f76b65ad6963b3881afd8ceaeef6021eeb
InDigitalEx <indigitale8@gmail.com>
2019-09-08 21:35:11 +0400

    v0.2

--- ARZ_NoCarSkill/main.cpp
+++ ARZ_NoCarSkill/main.cpp
@@ -4,44 +4,27 @@
 using std::string;
 
 SAMPFUNCS *SF = new SAMPFUNCS();
-static bool activeStatus = false;
+bool ActiveStatus = false;
 
-bool CALLBACK OutcomingData(stRakNetHookParams* params)
+bool CALLBACK IncomingRPC(stRakNetHookParams* params)
 {
-	if (activeStatus && params->packetId == PacketEnumeration::ID_VEHICLE_SYNC)
+	if (params->packetId == RPC_ScrSetVehicleVelocity)
 	{
-		stInCarData data;													// Создаем структуру
-		memset(&data, 0, sizeof(stInCarData));								// Обнуляем всё
-		byte packet;														// переменная для ID пакета
-
-		params->bitStream->ResetReadPointer();								// Сбрасываем оффсет чтения
-		params->bitStream->Read(packet);									// Читаем ID пакета
-		params->bitStream->Read((PCHAR)&data, sizeof(stInCarData));			// Читаем сам пакет
-		params->bitStream->ResetReadPointer();								// Снова сбрасываем оффсет
-
-		if (!data.sLeftRightKeys)
-		{
-			params->bitStream->ResetWritePointer();							// Сбрасываем оффсет записи
-			data.sLeftRightKeys = bool(rand() % 2) ? 128 : 65408;			// Записываем кнопку
-			params->bitStream->Write(packet);								// Записываем айди пакета
-			params->bitStream->Write((PCHAR)&data, sizeof(stInCarData));	// Отправляем пакет
+		bool value = ActiveStatus;
+		if (SF->getGame()->isKeyDown(160)) {
+			value = !value;
 		}
+		return !value;
 	}
 	return true;
 }
 
 void CALLBACK cmd_nocarskill(string params)
 {
+	ActiveStatus = !ActiveStatus;
 	string message = "[NCS]: {FFFFFF}Обход carskill'а ";
-	if (activeStatus == false) {
-		message += "{FF0000}включен.";
-	}
-	else {
-		message += "{00FF00}выключен.";
-	}
-
+	message += ActiveStatus ? "{FF0000}включен." : "{00FF00}выключен.";
 	SF->getSAMP()->getChat()->AddChatMessage(0x54AD10, message.c_str());
-	activeStatus = !activeStatus;
 }
 
 void CALLBACK MainLoop()
@@ -52,9 +35,9 @@ void CALLBACK MainLoop()
 		if (GAME && GAME->GetSystemState() == eSystemState::GS_PLAYING_GAME && SF->getSAMP()->IsInitialized())
 		{
 			initialized = true;
-			SF->getRakNet()->registerRakNetCallback(RakNetScriptHookType::RAKHOOK_TYPE_OUTCOMING_PACKET, OutcomingData);
+			SF->getRakNet()->registerRakNetCallback(RakNetScriptHookType::RAKHOOK_TYPE_INCOMING_RPC, IncomingRPC);
 			SF->getSAMP()->registerChatCommand("nocarskill", cmd_nocarskill);
-			SF->LogConsole("[ARZ]NoCarSkill by InDigital загружен. Активация: /nocarskill");
+			SF->LogConsole("{0x54AD10}[ARZ]NoCarSkill by InDigital загружен. {FFFFFF}Активация: /nocarskill");
 		}
 	}
 }
